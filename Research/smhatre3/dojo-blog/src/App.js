cae6e5e945b058d797b58883fa7436e9b3787ee2
shravani2035 <smhatre3@illinois.edu>
2023-03-02 20:57:44 -0600

    React tutorial

---
+++ Research/smhatre3/dojo-blog/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  const title = 'Welcome to the new blog';
+
+  return (
+    <div className="App">
+      <div className= "Content">
+        <h1>{ title }</h1>
+        <p>Here is a sample body of text to check functioning of VS code</p> 
+      </div>
+      <header className="App-header">
+      </header>
+    </div>
+  );
+} 
+
+export default App;
