4ce53ad2704e07194cc16d28b793de93910e8e64
ynarojpn <yeremias.njp@gmail.com>
2023-03-23 21:47:13 -0500

    resized board

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -3,7 +3,7 @@ import { Chessboard } from "react-chessboard";
 export default function App() {
   return (
     <div>
-      <Chessboard id="BasicBoard" />
+      <Chessboard id="BasicBoard" boardWidth={500} />
     </div>
   );
 }
