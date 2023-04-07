78a89407c96557d090e3726195598d9f779f37f1
apalu3 <apalu3@illinois.edu>
2023-04-06 20:46:38 -0500

    working chess board

--- Project/Backend/flask1.py
+++ Project/Backend/flask1.py
@@ -1,10 +1,14 @@
 from flask import Flask, redirect, url_for
-app = Flask(__name__)
+import chess
+import itertools
 
+app = Flask(__name__)
+board = chess.Board("rnbqkbnr/pppp1Qpp/8/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4")
 @app.route("/")
 def hello():
-    return "Hello Wrold!"
+    print(board) 
+    converted = str(board)
+    return (converted)
 
 if __name__ == "__main__":
     app.run()
-    
