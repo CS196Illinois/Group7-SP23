97c62b5aacc79eafad7250ef3e376e98f3d0749c
apalu3 <apalu3@illinois.edu>
2023-04-13 21:14:37 -0500

    got working chess engine windows onlt

--- Project/Backend/backend.py
+++ Project/Backend/backend.py
@@ -1,3 +1,16 @@
 import chess
-board = chess.Board("rnbqkbnr/pppp1Qpp/8/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4")
+import chess.engine
+board = chess.Board()
 print (board)
+print(board.legal_moves)
+
+d4 = chess.Move.from_uci("d2d4")
+board.push(d4)
+
+print(board)
+engine = chess.engine.SimpleEngine.popen_uci(r"C:\Users\anton\Documents\GitHub\Group7-SP23\Project\Backend\stockfish_15.1_win_x64_popcnt\stockfish-windows-2022-x86-64-modern.exe")
+#while not board.is_game_over():
+result = engine.play(board, chess.engine.Limit(time=0.1))
+board.push(result.move)
+engine.quit()
+print(board)
