0391a926a32ab7776a2c86b9352b18baca21d772
SpyrosKalleas <kalleas2@illinois.edu>
2023-04-20 21:06:27 -0500

    buttons
    i added the clear board and start position buttons

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -1,20 +1,33 @@
 import { Chessboard } from "react-chessboard";
+import { useState } from "react";
 
 export default function App() {
+  const [position, setPosition] = useState('start');
+  function clearBoard() {
+    setPosition({});
+  }
+  function startPosition() {
+    setPosition('start');
+  }
+  function updatePosition(updatedposition) {
+    setPosition(updatedposition);
+    console.log(updatedposition);
+  }
+  
   return (
     <><div>
-      <Chessboard id="BasicBoard" boardWidth={500} />
-
-      <button id="clearBoardBtn">Clear Board</button>
-      <button id="startPositionBtn">Start Position</button>
-      <button id="clearBoardInstantBtn">Clear Board Instant</button>
+      <Chessboard id="BasicBoard" boardWidth={500} position={position} getPositionObject={updatePosition}/>
+      
+      <button onClick={clearBoard}>Clear Board</button>
+      <button onClick={startPosition}>Start Position</button>
+      {/* <button onClick={getPosition}>Get Position</button> */}
 
     </div><script>
-        var board = Chessboard('myBoard', 'start')
+        {/* var board = Chessboard('myBoard', 'start') */}
 
-        $('#clearBoardBtn').on('click', board.clear)
+        {/* $('#clearBoardBtn').on('click', board.clear) */}
 
-        $('#startPositionBtn').on('click', board.start)
+        {/* $('#startPositionBtn').on('click', board.start) */}
 
       </script></>
     
