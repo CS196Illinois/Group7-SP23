b881f352ed15048ba8e50b24f8c4b8fb94859d8f
SpyrosKalleas <kalleas2@illinois.edu>
2023-04-26 13:17:00 -0500

    added submit white and black buttons

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -2,24 +2,46 @@ import { Chessboard } from "react-chessboard";
 import { useState } from "react";
 
 export default function App() {
-  const [position, setPosition] = useState('start');
+  const [position, setPosition] = useState('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR');
   function clearBoard() {
     setPosition({});
   }
   function startPosition() {
-    setPosition('start');
+    setPosition('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR');
   }
   function updatePosition(updatedposition) {
     setPosition(updatedposition);
     console.log(updatedposition);
   }
   
+  let bturn = {"turn":"black",position};
+  let wturn = {"turn":"white",position};
+  function submitPositionB() {
+   fetch('http://localhost:3000/', {
+     method: 'POST',
+     headers: {"Content-Type": "application/json" },
+     body: JSON.stringify(bturn)
+   }).then(() => {
+     console.log(bturn);
+   })
+  }
+  function submitPositionW() {
+    fetch('http://localhost:3000/', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json" },
+      body: JSON.stringify(wturn)
+    }).then(() => {
+      console.log(wturn);
+    })
+   }
   return (
     <><div>
       <Chessboard id="BasicBoard" boardWidth={500} position={position} getPositionObject={updatePosition}/>
       
       <button onClick={clearBoard}>Clear Board</button>
       <button onClick={startPosition}>Start Position</button>
+      <button onClick={submitPositionB}>Submit Position for Black</button>
+      <button onClick={submitPositionW}>Submit Position for White</button>
       {/* <button onClick={getPosition}>Get Position</button> */}
 
     </div><script>
