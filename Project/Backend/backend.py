882dfb08ab33229b23b6b123e863eeb5b475d0fa
apalu3 <apalu3@illinois.edu>
2023-04-25 20:14:31 -0500

    working with relative path

--- Project/Backend/backend.py
+++ Project/Backend/backend.py
@@ -1,14 +1,18 @@
 import chess
 import chess.engine
+from pathlib import Path
 board = chess.Board()
 print (board)
 print(board.legal_moves)
+
+
 
 d4 = chess.Move.from_uci("d2d4")
 board.push(d4)
 
 print(board)
-engine = chess.engine.SimpleEngine.popen_uci(r"C:\Users\anton\Documents\GitHub\Group7-SP23\Project\Backend\stockfish_15.1_win_x64_popcnt\stockfish-windows-2022-x86-64-modern.exe")
+engine = chess.engine.SimpleEngine.popen_uci("Project\Backend\stockfish\Stockfish.exe")
+
 #while not board.is_game_over():
 result = engine.play(board, chess.engine.Limit(time=0.1))
 board.push(result.move)
